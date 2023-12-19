65c10a577d7dbcaecdf8e78a0c53c2b9483d6db6
shreya-mishra <shreyamishra040398@gmail.com>
2023-12-19 12:48:11 +0530

    fetched response from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://my-json-server.typicode.com/codebuds-fk/chat/chats")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Chat filter option
+      ChatList
+      selected Chat */}
     </div>
   );
 }
